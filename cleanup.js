98887f865412295c9c9c256d74a04b9103fbedc2
Laura Wrubel <lwrubel@gwu.edu>
2015-02-27 20:33:05 -0500

    improvements to cleanup script

--- cleanup.js
+++ cleanup.js
@@ -1,27 +1,18 @@
 // removes sets of documents not relevant to the app
 
-conn = new Mongo();
-db = conn.getDB("disasters");
+db = new Mongo().getDB("disasters")
 
-// images of agency activities, mostly not of disasters themselves
-db.hurricanes.remove({
-    "sourceResource.collection.id" : "dfd693cc7db3865a78d62523b3d3f124" 
-    });
-
-// no thumbnail provided
-db.floods.remove({
-    "object" : null 
-    });
-
-// thumbnail is of GPO logo; no image available
-db.floods.remove({ 
-    "object" : "http://fdlp.gov/images/gpo-tn.jpg"
-    });
-db.earthquakes.remove({ 
-    "object" : "http://fdlp.gov/images/gpo-tn.jpg"
-    });
-db.hurricanes.remove({ 
-    "object" : "http://fdlp.gov/images/gpo-tn.jpg"
-    });
-
+var deletes = [ 
+    {msg: "non-disaster images of agencies", coll: "hurricanes", fields: { "sourceResource.collection.id" : "dfd693cc7db3865a78d62523b3d3f124" }}, 
+    {msg: "no thumbnail", coll: "floods", fields: { object: null }}, 
+    {msg: "image is GPO logo", coll: "floods", fields: { object: "http://fdlp.gov/images/gpo-tn.jpg" }}, 
+    {msg: "image is GPO logo", coll: "earthquakes", fields: { object: "http://fdlp.gov/images/gpo-tn.jpg" }}, 
+    {msg: "image is GPO logo", coll: "hurricanes", fields: { object: "http://fdlp.gov/images/gpo-tn.jpg" }} 
+    ]
+    
+for (var i = 0; i < deletes.length; i++) {
+    var col = deletes[i].coll
+    print("deleting docs from " + col + ": " + deletes[i].msg)
+    db[col].remove( deletes[i].fields )
+    }
 
