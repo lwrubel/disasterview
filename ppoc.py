705f4c37a9fd06566b6c256e4f5e68f0f09176b6
Laura Wrubel <lwrubel@gwu.edu>
2015-03-03 11:12:18 -0500

    request metadata from LC Prints and Photographs online catalog

---
+++ ppoc.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import re
+from urlparse import *
+from datetime import datetime
+from config import data_dir
+
+today = datetime.now()
+              
+url = 'http://loc.gov/pictures/search/?'
+query = {'c':50, 'sp': 1, 'fo':'json', 'co!':'hh', #exclude HAER, HABS  
+         'fa': 'displayed:anywhere', 'fi': 'subject' }
+
+def getdata(disasters):
+    for subject in disasters:
+        query['q'] = subject
+        r = requests.get(url, params=query)
+        response = r.json()
+        createfile(response['results'], subject, query['sp'])
+        
+        while response['pages']['next']: # get rest of pages
+            link = response['pages']['next'] + '&fo=json'
+            r = requests.get(link)
+            response = r.json()
+            params = parse_qs(urlparse(r.url).query) # creates dict out of params in url
+            createfile(response['results'], subject, params['sp'][0])
+ 
+# write response to one file per subject           
+def createfile(data, subject, page):
+    e = re.compile('\W.*') 
+    subject = e.sub('', subject)
+    filename = data_dir + '{0}-{1}-{2}-ppoc.json'.format(subject,today.isoformat(),page)
+    f = open(filename, 'w')
+    f.write(json.dumps(data)) 
+    f.close()
+
+disasters = ['earthquakes','hurricanes','floods','forest%20fires']
+
+getdata(disasters)
+        
