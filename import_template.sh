f7fab3eb374ce8ea91a9b670fe2a85002f127aad
Laura Wrubel <lwrubel@gwu.edu>
2015-03-06 15:15:28 -0500

    use mongodb default db path instead

--- import_template.sh
+++ import_template.sh
@@ -2,7 +2,6 @@
 # run from project directory  
 
 DATA_DIR=pathtofiles
-DB_PATH=pathtodb
 DATABASE=yourdbname
 FILES=$DATA_DIR/*.json
 
@@ -19,9 +18,9 @@ do
 # strip out longest substring starting with "-" from the back of the string, i.e. all but subject name
   COLLECTION=${COLLECTION%%-*}
 
-  echo "doing: mongoimport --dbpath $DATA_DIR --db $DATABASE --collection $COLLECTION --file $f --jsonArray"
+  echo "doing: mongoimport --db $DATABASE --collection $COLLECTION --file $f --jsonArray"
 
-  mongoimport --dbpath $DB_PATH --db $DATABASE --collection $COLLECTION  --file $f --jsonArray
+  mongoimport --db $DATABASE --collection $COLLECTION  --file $f --jsonArray
   
   echo "Done $f from $COLLECTION"
 done
