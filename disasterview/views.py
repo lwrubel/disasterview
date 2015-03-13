027440b1b01622be5f0e0d38de824650e301e90c
Laura Wrubel <lwrubel@gwu.edu>
2015-03-13 16:15:09 -0400

    more experimenting with views

--- disasterview/views.py
+++ disasterview/views.py
@@ -13,13 +13,23 @@ db = connect()
 def single_disaster():
     hurricane = db.hurricanes.find_one()
     title = hurricane['title']
-    thumb = hurricane['object']
-    return render_template('index.html', title=title, thumb=thumb)
+    thumbnail = hurricane['thumbnail']
+    return render_template('index.html', title=title, thumbnail=thumbnail)
     
-@app.route('/hurricanes/')
-def browse_images():
-    items = list(db.hurricanes.find({}))
-    
+@app.route('/disasters/hurricanes/')
+def browse_hurricane_images():
+    items = list(db.hurricanes.find())
     return render_template('hurricanes.html', items=items) 
+	#needs thumbnail, maybe later include more 
 
+@app.route('/disasters/floods/')
+def floods_view():    
+    items = list(db.floods.find({}))
+    return render_template('hurricanes.html', items=items)
+
+
+@app.route('/disasters/forestfires/')
+def forestfires_view():    
+    items = list(db.forest.find({}))
+    return render_template('hurricanes.html', items=items)
 
