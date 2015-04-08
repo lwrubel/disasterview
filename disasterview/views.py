a92bec779161f84b24bb7a1538283c3ccf9ac99a
Laura Wrubel <lwrubel@gwu.edu>
2015-04-08 12:22:47 -0400

    implemented item selection and view of selections, styling updates

--- disasterview/views.py
+++ disasterview/views.py
@@ -1,5 +1,5 @@
 from disasterview import app
-from flask import render_template
+from flask import render_template, request
 from pymongo import MongoClient
 import math
 
@@ -16,6 +16,12 @@ names = {'earthquakes':'earthquakes','floods': 'floods','forest': 'forest fires'
 def return_cover():    
     return render_template('main.html')
 
+
+@app.route('/credits/')
+def show_credits():
+    return render_template('about.html')
+
+
 @app.route('/disasters/<event_type>/', defaults={'n': 1})
 @app.route('/disasters/<event_type>/<int:n>/')
 def browse_images_pages(event_type, n):
@@ -30,6 +36,7 @@ def browse_images_pages(event_type, n):
         
     return render_template('events.html', items=page, event_type=event_type, 
         nopages=nopages, pagenum=n, label=names[event_type])
+  
     
 @app.route('/map/')
 def show_map(): 
@@ -45,11 +52,17 @@ def show_map():
                     'url': location['nativeView'], 'thumbnail': location['thumbnail'], 'disaster': disaster})
     return render_template('map.html', items=items)
 
-@app.route('/picker/')
+
+@app.route('/items/')
 def picklist():
     items = []
-    disasters = ['earthquakes','floods','forest','hurricanes']
-
-    return render_template('picker.html', items=items)
+    for key in request.args.keys():
+        if key != "count":
+            parts = key.split('_')
+            event_type = parts[1]
+            record_id = request.args.get(key)
+            item = db[event_type].find_one({'id' : record_id})
+            items.append(item)
+    return render_template('items.html', items=items)
     
     
